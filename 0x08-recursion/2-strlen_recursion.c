d3c072ff690c50a1ff8233868db65258735468f9
Yona2000Jn <yonajames2000@gmail.com>
2022-09-28 23:46:15 +0300

    2 Task

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,13 +2,18 @@
 
 /**
  * _strlen_recursion - returns the length of a string
- * @s: string to calculate the length of
- *
- * Return: length of the string
+ * @s: pointer the string
+ * Return: int
  */
+
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
-		return (0);
-	return (1 + _strlen_recursion(s + 1));
+int n = 0;
+
+if (*s > '\0')
+{
+	n += _strlen_recursion(s + 1) + 1;
+}
+
+return (n);
 }
